f1cadbcf3189392880951519ce73dd647484e1da
Jian Xu <jianxu1@microsoft.com>
2024-03-23 10:30:35 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,29 @@
-/// This program monitors the specified processes and closes their windows if they are found.
-/// The process names to be monitored are hard-coded in the function.
-/// To monitor a process, use Spy++ to get the window name.
-///
 use std::env;
 use std::thread;
 use std::time::Duration;
-use windows::core::s;
+use windows::core::PCSTR;
 use windows::Win32::Foundation::{LPARAM, WPARAM};
 use windows::Win32::UI::WindowsAndMessaging::{FindWindowA, SendMessageA, WM_CLOSE};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // Skip the first argument which is the program name itself
+    let window_names = &args[1..];
+
     println!("{} is monitoring...", args[0]);
+    println!("Window names to close: {:?}", window_names);
 
     unsafe {
         loop {
-            // to monitor the process, you can use Spy++ to get the window name
-            let h_wnd = FindWindowA(None, s!("Message"));
-            let h_wnd_agent = FindWindowA(None, s!("Microsoft.Flow.RPA.Agent.exe - System Error"));
-            let h_wnd_pad_automationserver =
-                FindWindowA(None, s!("PAD.AutomationServer.exe - System Error"));
-            let h_wnd_pad_robot = FindWindowA(None, s!("PAD.Robot.exe - Application Error"));
-            if h_wnd.0 != 0 {
-                SendMessageA(h_wnd, WM_CLOSE, WPARAM::default(), LPARAM::default());
-            }
-            if h_wnd_agent.0 != 0 {
-                SendMessageA(h_wnd_agent, WM_CLOSE, WPARAM::default(), LPARAM::default());
-            }
-            if h_wnd_pad_automationserver.0 != 0 {
-                SendMessageA(
-                    h_wnd_pad_automationserver,
-                    WM_CLOSE,
-                    WPARAM::default(),
-                    LPARAM::default(),
-                );
-            }
-            if h_wnd_pad_robot.0 != 0 {
-                SendMessageA(
-                    h_wnd_pad_robot,
-                    WM_CLOSE,
-                    WPARAM::default(),
-                    LPARAM::default(),
-                );
+            for window_name in window_names {
+                // Convert the Rust string to a C-style string
+                let window_name_cstr = PCSTR(window_name.as_ptr());
+
+                let hwnd = FindWindowA(None, window_name_cstr);
+                if hwnd.0 != 0 {
+                    SendMessageA(hwnd, WM_CLOSE, WPARAM::default(), LPARAM::default());
+                }
             }
 
             thread::sleep(Duration::from_millis(500));
